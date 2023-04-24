7cc186b95429fc6a35e9bba33ab558f677fd86ed
diogotvf7 <up202108752@g.uporto.pt>
2023-04-24 01:10:35 +0100

    Cleaned comments

--- pages/display_tickets.php
+++ pages/display_tickets.php
@@ -14,35 +14,6 @@
   require_once (__DIR__ . '/../database/ticket.class.php');
 
   $db = getDatabaseConnection();
-  /* ACTUAL CODE */
-  // drawHeader([]);
-  // $user1 = new Admin(1, 'Admin', 'admin', 'admin@admin.com', '123456');
-  // $user2 = new Agent(2, 'Agent', 'agent', 'agent@agent.com', '123456');
-  // $user3 = new Client(3, 'Client', 'client', 'client@client.com', '123456');
-  // drawSideBar($user1);
-  // drawFooter();
-  
-  // // // // // // // // // // // // // // // // // // // // 
-
-  /* ASSURING TICKETS ARE BEING FETCHED */
-  // $ticketsIds = Ticket::getAllTickets($db);
-  // var_dump($ticketsIds);
-  // echo '<br>';
-  // echo 'number of tickets -> ' . sizeof($ticketsIds);
-  // echo '<br>';
-  // foreach ($ticketsIds as $ticketId) {
-  //   $ticket = Ticket::getTicketData($db, $ticketId);
-  //   var_dump($ticket);
-  // }
-
-  // // // // // // // // // // // // // // // // // // // // 
-
-  /* Display a ticket for testing */
-
-  // echo 'Attempting to create ticket object!<br>';
-  // $ticket = new Ticket(1, 'Issue with product', 'I am having trouble with my product', 'Open', 1, 3, 1, '2022-01-01 12:00:00');
-  // echo 'Ticket object created!<br>';
-
 
   drawHeader(['ticket_colors'], ['style']);
   $user1 = new Admin(1, 'Admin', 'admin', 'admin@admin.com', '123456');
